a3f58d7ac85009e796e42a60fa914e8078b773f9
aightech <long.devillard@gmail.com>
2020-01-17 13:47:51 +0100

    updated

---
+++ CMakeFiles/canopen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/canopen.dir/src/canopen_socket.cpp.o"
+  "CMakeFiles/canopen.dir/src/main.cpp.o"
+  "canopen.pdb"
+  "canopen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/canopen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
