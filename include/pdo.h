3e569b7e016196ed62b66c4be70b5a217b985ef8
Florian Richer <florian.richer@sorbonne-universite.fr>
2020-03-13 11:47:22 +0100

    Allow building a PDO object from its function code and payload

--- include/pdo.h
+++ include/pdo.h
@@ -4,11 +4,22 @@
 #include "message.h"
 
 namespace CANopen {
-class PDOMessage : public Message
-{
+class PDOMessage : public Message {
 public:
+    enum PDOFunctionCode : uint32_t {
+        PDO1Transmit = Message::PDO1Transmit,
+        PDO1Receive = Message::PDO1Receive,
+        PDO2Transmit = Message::PDO2Transmit,
+        PDO2Receive = Message::PDO2Receive,
+        PDO3Transmit = Message::PDO3Transmit,
+        PDO3Receive = Message::PDO3Receive,
+        PDO4Transmit = Message::PDO4Transmit,
+        PDO4Receive = Message::PDO4Receive,
+    };
+
     PDOMessage() = default;
     PDOMessage(const can_frame& other);
+    PDOMessage(PDOFunctionCode fn, uint8_t node_id, Payload payload);
 };
 }
 
--- src/pdo.cpp
+++ src/pdo.cpp
@@ -5,4 +5,10 @@ PDOMessage::PDOMessage(const can_frame& other)
     : Message(other)
 {
 }
+
+PDOMessage::PDOMessage(PDOFunctionCode fn, uint8_t node_id, Payload payload)
+    : Message(fn + node_id, payload)
+{
+}
+
 }
