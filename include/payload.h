805ebb46b75c1acebfdae968f31633c297792053
adev <devillard@isir.upmc.fr>
2020-03-20 15:54:14 +0100

    add constructor from primary data type to be able to send message with a payload from just an number.

--- include/payload.h
+++ include/payload.h
@@ -9,46 +9,55 @@
 
 namespace CANopen {
 class Payload : public std::vector<uint8_t> {
-public:
+    public:
     Payload() = default;
-    Payload(const Payload&) = default;
-    Payload(const std::vector<uint8_t>& other);
-    Payload& operator=(const Payload&) = default;
+    Payload(const Payload &) = default;
+    Payload(const std::vector<uint8_t> &other);
 
     template <typename T>
-    static Payload from_data(const T& value)
-    {
+    Payload(T value) {
+        for(int i = 0; i < sizeof(T); i++)
+            push_back(*((uint8_t *)(&value) + i));
+    }
+
+    Payload &
+    operator=(const Payload &) = default;
+
+    template <typename T>
+    static Payload
+    from_data(const T &value) {
         Payload ret;
-        for (int i = 0; i < sizeof(T); ++i) {
+        for(int i = 0; i < sizeof(T); ++i) {
             ret.push_back(value >> (8 * i));
         }
         return ret;
     }
 
     template <typename T1, typename... Ts>
-    static Payload from_data(const T1& value, const Ts&&... others)
-    {
+    static Payload
+    from_data(const T1 &value, const Ts &&... others) {
         return from_data(others...).push_back(from_data(value));
     }
 
     template <typename T>
-    T& value(unsigned begin=0)
-  {
-    return *(T*)(data()+begin);
-  }
+    T &
+    value(unsigned begin = 0) {
+        return *(T *)(data() + begin);
+    }
 
     template <typename T>
-    Payload& operator<<(T& value)
-    {
-      for(int i =0; i<sizeof(T);i++)
-	push_back(*((uint8_t*)(&value)+i));
+    Payload &
+    operator<<(T &value) {
+        for(int i = 0; i < sizeof(T); i++)
+            push_back(*((uint8_t *)(&value) + i));
         return *this;
     }
 
     operator std::string() const;
 };
-}
+} // namespace CANopen
 
-std::ostream& operator<<(std::ostream& out, const CANopen::Payload& p);
+std::ostream &
+operator<<(std::ostream &out, const CANopen::Payload &p);
 
 #endif // _CANOPEN_PAYLOAD_H_
